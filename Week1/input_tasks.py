2124d008c37158d18588bd324f714f3c1f58569a
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-09-27 15:07:40 +0100

    User can now select robot's eyes

--- Week1/input_tasks.py
+++ Week1/input_tasks.py
@@ -4,3 +4,13 @@ name = input()
 print(f"It is nice to meet you {name}")
 #An f-string can be used to format output allowing us to combine our message with the value of our variable
 
+
+# Display a robot with user's eyes
+print("Please enter a character for the eye")
+eye = input()
+print("The robots expression is as follows:")
+print("\\        /")
+print("##########")
+print(f"# {eye}    {eye} #")
+print("#  ____  #")
+print("##########")
