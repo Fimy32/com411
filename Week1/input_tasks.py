8a415d148544d667fae0b6b3371fb1daeecd5bad
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-09-27 15:00:20 +0100

    Input for user's name added

---
+++ Week1/input_tasks.py
@@ -0,0 +1,6 @@
+# Ask user to enter their name
+print("What is your name?")
+name = input()
+print(f"It is nice to meet you {name}")
+#An f-string can be used to format output allowing us to combine our message with the value of our variable
+
