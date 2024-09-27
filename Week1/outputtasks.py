57bbbdc3feeff14c02e3da74ac9e5372e3d9419a
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-09-27 14:46:48 +0100

    Escape Characters Added

---
+++ Week1/outputtasks.py
@@ -0,0 +1,10 @@
+# Display message to the screen
+print("Welcome to COM411!")
+print("In week 1 we will learn...\n\n...How to use Git and GitHub\n...How to output to the screen\n...How to get user input\n\nI hope you are enjoying the lessons thus far!")
+
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
--- repo_test.py
+++ repo_test.py
@@ -1,1 +1,8 @@
-print("Hello World")
+for i in range(1,10):
+
+    if (i < 5):
+        print("Hello World")
+
+    else:
+
+        print(i)
