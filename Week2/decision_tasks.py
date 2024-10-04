b4235daa0294bd69aa7f442697461b1f5431b722
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-04 14:07:41 +0100

    Tested If Else

--- Week2/decision_tasks.py
+++ Week2/decision_tasks.py
@@ -1,6 +1,16 @@
-#If statement Demo
+#IF statement Demo
 
 book_Type = input("What type of book do you like?")
 if book_Type == "adventure":
     print("I like",book_Type,"books!")
+print("Finished reading book")
+
+
+#IF ELSE statement Demo
+
+activity_Type = input("Please enter an activity.")
+if activity_Type == "calculate":
+    print("Performing calculations...")
+else:
+    print("Performing",activity_Type + "...")
 print("Finished reading book")
