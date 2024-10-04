8a3c253eebfd9817aa64e0db6c29a399c46b62f0
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-04 14:52:23 +0100

    Nested if

--- Week2/nested_if.py
+++ Week2/nested_if.py
@@ -9,3 +9,31 @@ if book_type == "soft":
         print("Soft covers with coils or stitches are great for short books")
 if book_type == "hard":
     print("Books with hard covers can be more expensive!")
+
+#Multiple nested Decision
+
+position = input("Where should I look?")
+
+if (position == "in the bedroom"):
+    position = input("Where in the bedroom should I look?")
+    if (position == "under the bed"):
+        print("Found some shoes but no phone")
+    else:
+        print("Found some mess but no phone")
+
+if (position == "in the bathroom"):
+    position = input("Where in the bathroom should I look?")
+    if (position == "in the bathtub"):
+        print("Found a rubber duck but no phone")
+    else:
+        print("Found bathroom stuff but no phone")
+
+if (position == "in the living room"):
+    position = input("Where in the living room should I look?")
+    if (position == "on the table"):
+        print("Yes! I found my phone!")
+    else:
+        print("Found some stuff but no phone")
+
+else:
+    print("I do not know where that is but I will keep looking.")
