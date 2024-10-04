42979da3c9f602ef0f25873406fdc3f7e5179041
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-04 15:00:40 +0100

    Demoed And Operator

--- Week2/multiple_conditions.py
+++ Week2/multiple_conditions.py
@@ -8,3 +8,13 @@ elif (adventure == "long") or (adventure == "safe"):
     print("Taking the safe route!")
 else:
     print("Not sure which route to take.")
+
+#AND operator
+
+hear = input("What did I hear?")
+see = input("What did I see?")
+print(hear,see)
+if (hear == "grrr") and (hear == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue.")
