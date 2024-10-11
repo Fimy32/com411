4c9ecee7f850dfd494ec56df48c0ee06765be6e9
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-11 13:58:56 +0100

    Added more while loop

--- Week3/while_loops.py
+++ Week3/while_loops.py
@@ -10,6 +10,7 @@
 #     print("Remove Apple")
 #     removed_apples += 1
 
+
 # #While loop with a counter
 #
 # obstacle_num = int(input("How many obstacles should I avoid?"))
@@ -23,19 +24,37 @@
 #     print("Avoiding...Done!", current_obstacle,"avoided.")
 #
 
-#While loop with ASCII art
 
-bars_to_charge = int(input("How many bars do you want to charge?"))
+# #While loop with ASCII art
+#
+# bars_to_charge = int(input("How many bars do you want to charge?"))
+#
+# print(bars_to_charge,"\n")
+#
+# bars_charged = 0
+#
+# while bars_charged < bars_to_charge:
+#
+#     bars_charged += 1
+#
+#     print("Charging:", "[=]" * bars_charged)
+#
+# print("The battery is fully charged.")
 
-print(bars_to_charge,"\n")
 
-bars_charged = 0
+#While loop reading users input
 
-while bars_charged < bars_to_charge:
+phrase = str(input("Enter a phrase:"))
 
-    bars_charged += 1
+print(phrase,"\n")
 
-    print("Charging:", "[=]" * bars_charged)
+phrase_number = len(phrase)
 
-print("The battery is fully charged.")
+counter = 0
+
+while counter < phrase_number:
+
+    counter += 1
+
+    print("Hi", end=" ")
 
