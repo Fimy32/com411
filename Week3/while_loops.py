01d7ade2c3eef172fb84e08c3aada21afe1faebf
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-11 13:48:22 +0100

    While loop with a counter

--- Week3/while_loops.py
+++ Week3/while_loops.py
@@ -1,14 +1,23 @@
-#Simple While Loop
-
-apples = int(input("How many apples should I remove?"))
-
-print(apples)
-
-removed_apples = 0
-
-while apples > removed_apples:
-    print("Remove Apple")
-    removed_apples += 1
+# #Simple While Loop
+#
+# apples = int(input("How many apples should I remove?"))
+#
+# print(apples)
+#
+# removed_apples = 0
+#
+# while apples > removed_apples:
+#     print("Remove Apple")
+#     removed_apples += 1
 
 #While loop with a counter
 
+obstacle_num = int(input("How many obstacles should I avoid?"))
+
+current_obstacle = 0
+
+while current_obstacle < obstacle_num:
+
+    current_obstacle = current_obstacle + 1
+
+    print("Avoiding...Done!", current_obstacle,"avoided.")
