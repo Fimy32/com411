69060cdfbb4bc6d2731d6eff641e350334bd00af
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-18 13:57:46 +0100

    Expanded adventure game

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -1,5 +1,21 @@
+#Text based Adventure game
+
+#Listen Function
+
 def listen():
     sound = input("Enter a sound")
     print("That was a loud",sound + "!")
 
+#Identify Function
+
+def identify():
+    obstacle = input("What do you see?")
+    if obstacle != "a large boulder":
+        print("We should be fine.")
+    else:
+        print("We should run!")
+
 listen()
+
+identify()
+
