c75001ca61d90653d1620b2614d740e7ec2ca7f3
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-18 14:10:38 +0100

    more game

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -14,8 +14,21 @@ def identify():
         print("We should be fine.")
     else:
         print("We should run!")
+#Escape_by function
+
+def escape_by(plan):
+    if plan == "jumping over":
+        print("We cannot escape that way! The boulder is moving too fast!")
+    elif plan == "cross bridge ahead":
+        print("That just might work lets go!")
+    else:
+        print("We cannot escape that way! The boulder is in the way!")
 
 listen()
 
 identify()
+
+escape_by("jumping over")
+escape_by("running around")
+escape_by("cross bridge ahead")
 
