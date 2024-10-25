c90119da47bfa68e013b5fba2685ec3952792e17
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-25 14:36:02 +0100

    Demoed
    Tuples returning

--- Week 5/tuple_tasks.py
+++ Week 5/tuple_tasks.py
@@ -2,10 +2,22 @@
 
 def likelihood():
     likelihoods = (50, 38, 27, 99 , 4)
-    return min(likelihoods)
+    return likelihoods
 def run_task1():
-    print("Minimum likelihood of failing:", str(likelihood()) + "%")
+    print("Minimum likelihood of failing:", str(min((likelihood()))) + "%")
 
 
 if __name__ == "__main__":
     run_task1()
+
+#Returning Tuples in functions
+
+def likelihood_min_max():
+    return min(likelihood()), max(likelihood())
+
+def run_task2():
+    print("Minimum likelihood of falling:", str(likelihood_min_max()[0]) + "%\n" + "Maximum likelihood of falling:", str(likelihood_min_max()[1]) + "%")
+
+
+if __name__ == "__main__":
+    run_task2()
