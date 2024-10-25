8dc165fa0b56319ef32ad220d3504c6068b407ee
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-10-25 13:42:34 +0100

    Demoed indexing a list

--- Week 5/list_tasks.py
+++ Week 5/list_tasks.py
@@ -1,3 +1,5 @@
+#Simple List
+
 def directions():
     steps = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
     return steps
@@ -7,3 +9,20 @@ def run_task1():
 
 if __name__ == "__main__":
     run_task1()
+
+
+
+#Simple list with varying data types
+
+def movements():
+    path = ["Move forward", 10, "Move backward", 5, "Move left", 3, "Move right", 1]
+    return path
+
+def run_task2():
+    print("Moving...")
+    temp_movements = movements()
+    for i in range(1, len(temp_movements) - 1,2):
+        print(temp_movements[i],"for",temp_movements[i+1],"steps")
+
+if __name__ == "__main__":
+    run_task2()
