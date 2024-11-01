063dc7106d795258dec524237301f588065df226
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-11-01 14:49:28 +0000

    Updated code base

--- Week6/txt_file_tasks.py
+++ Week6/txt_file_tasks.py
@@ -1,14 +1,36 @@
 import os
 
-def cwd():
-    path = os.getcwd()
-    print(f"Current Working Directory: {path}")
-    print("The directory contains the following files:")
-    for file in os.listdir(path):
-        print(file)
+# def cwd():
+#     path = os.getcwd()
+#     print(f"Current Working Directory: {path}")
+#     print("The directory contains the following files:")
+#     for file in os.listdir(path):
+#         print(file)
+#
+#
+# def run():
+#     cwd
+#
+# run()
 
 
-def run():
-    cwd
 
-run()
+def display_chars(path,count):
+    with open(path, "r") as file:
+        print(file.read(int(count)))
+
+def display_line(path):
+    with open(path, "r") as file:
+        print(file.readline())
+
+def display_text(path):
+    with open(path, "r") as file:
+        print(file.read())
+
+def run_task2():
+    display_chars("library.txt", "10")
+    display_line("library.txt")
+    display_text("library.txt")
+
+if __name__ == "__main__":
+    run_task2()
