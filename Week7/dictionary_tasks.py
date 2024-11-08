7f6611731e7766e36a97bdcade1b2b3183807d96
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-11-08 14:59:50 +0000

    Demoed iterating through a dictionary

--- Week7/dictionary_tasks.py
+++ Week7/dictionary_tasks.py
@@ -6,5 +6,28 @@ def patterns():
 
 def run():
     print(patterns())
+    print("\n")
+    display_keys(patterns())
+    display_values(patterns())
+    display_items(patterns())
+
+
+
+
+#Iterating through a dictionary
+
+def display_keys(data):
+    for key in data:
+        print(key, "\n")
+
+def display_values(data):
+    for key in data:
+        print(data[key], "\n")
+
+def display_items(data):
+    for key in data:
+        print(key, print(data[key]), "\n")
 
 run()
+
+
