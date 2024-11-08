01583123061e9d903817aa0189c2da447d87505c
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-11-08 14:22:32 +0000

    Demoed removing from a set

--- Week7/settasks.py
+++ Week7/settasks.py
@@ -1,26 +1,54 @@
-#Simple Sets
+# #Simple Sets
+#
+# def observed():
+#     observations = {"Car","Sky Scraper", "Bike", "House"}
+#     return observations
+#
+#
+# def run_task1():
+#     print(observed())
+#
+#
+# run_task1()
+#
+#
+# #Sets with duplicates
+#
+# def observed_items():
+#     observations = []
+#     for i in range(7):
+#         observations.append(input("Enter Observation"))
+#     return observations
+#
+# def run_task2():
+#     print("Counting Observations")
+#     observeditems = observed_items()
+#     observed_set = set()
+#     for element in observeditems:
+#         observed_set.add((element, observeditems.count(element)))
+#     print(observed_set)
+#
+# run_task2()
 
-def observed():
-    observations = {"Car","Sky Scraper", "Bike", "House"}
-    return observations
 
-
-def run_task1():
-    print(observed())
-
-
-run_task1()
-
-
-#Sets with duplicates
+#Removing From A set
 
 def observed_items():
-    observations = set()
-    for i in range(7):
-        observations.add(input("Enter Observation"))
+    observations = []
+    for i in range(5):
+        observations.append(input("Enter Observation"))
     return observations
 
-def run_task2():
-    print(observed_items())
+def remove_observations(observations):
+    while input("Would you like to remove observations? [y/n]") == "y":
+        observations.remove(input("Enter Observation"))
+    return observations
 
-run_task2()
+def run_task3():
+    observeditems = remove_observations(observed_items())
+    observed_set = set()
+    for element in observeditems:
+        observed_set.add((element, observeditems.count(element)))
+    print(observed_set)
+
+run_task3()
