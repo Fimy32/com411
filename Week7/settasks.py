caeb77b1fc307b2dff8c1d6eab71a16597928acb
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-11-08 13:54:19 +0000

    Demoed using duplicates in sets

--- Week7/settasks.py
+++ Week7/settasks.py
@@ -5,7 +5,22 @@ def observed():
     return observations
 
 
-
-def run_task1() {
+def run_task1():
     print(observed())
-}
+
+
+run_task1()
+
+
+#Sets with duplicates
+
+def observed_items():
+    observations = set()
+    for i in range(7):
+        observations.add(input("Enter Observation"))
+    return observations
+
+def run_task2():
+    print(observed_items())
+
+run_task2()
