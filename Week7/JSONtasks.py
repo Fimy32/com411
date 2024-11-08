6c7c0967f97963473d98bd633725037b5a3119fb
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-11-08 15:56:47 +0000

    updated codebase

--- Week7/JSONtasks.py
+++ Week7/JSONtasks.py
@@ -3,6 +3,6 @@ import json
 def read(path):
     with open(path, 'r') as file:
         data = json.load(file)
-        return "Location", data['location'], "Population Size", data['population'], data['bots']["name"], "has the following stats:", data['bots']
+        return "Location", data['location'], "\nPopulation Size", data['population'], "\n" + data["bots"], "has the following stats:", data['bots']
 
 print(read("futurama.json"))
