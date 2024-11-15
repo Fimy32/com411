1e605729df69870f5e07ab069896effbd99fe9b4
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-11-15 13:43:31 +0000

    Demoed basic data visualisation

---
+++ Week8/plots.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+# #Basic Data Visualisation
+# def display_line(xvalues,yvalues):
+#     plt.xlabel("X Value")
+#     plt.ylabel("Y Value")
+#     plt.plot(xvalues, yvalues)
+#     plt.show()
+#
+# def run_task1():
+#     display_line([1,2,3,4,5],[1,4,9,16,25])
+#
+# run_task1()
+
+#More complex data visulisation
---
+++ Week8/test.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as matplot
+
+x = [0,2,4,6,8,10]
+
+y = [0,20,40,60,80,100]
+
+matplot.xlabel("X Value")
+matplot.ylabel("Y Value")
+matplot.plot(x,y)
+matplot.show()
