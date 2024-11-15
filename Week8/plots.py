35806ac836c7d22821b861eed05783486fbc19b1
BEN HAIGH-SHIPP <0haigb36@solent.ac.uk>
2024-11-15 14:14:45 +0000

    Showed how data can be inputted from the user using functions

--- Week8/plots.py
+++ Week8/plots.py
@@ -29,3 +29,28 @@ import matplotlib.pyplot as plt
 # medium()
 # large()
 # plt.show()
+
+
+#User Inputed Data Visualisation
+
+def coordinate():
+    return input("Enter an X"),input("Enter an Y")
+
+def path():
+    print("Retrieving Path")
+    x_values = []
+    y_values = []
+    for i in range(4):
+        data = coordinate()
+        x_values.append(data[0])
+        y_values.append(data[1])
+    return x_values,y_values
+
+def run_task3():
+    values = path()
+    plt.plot(values[0],values[1],"ro--")
+    plt.xlabel("X Values")
+    plt.ylabel("Y Values")
+    plt.show()
+
+run_task3()
